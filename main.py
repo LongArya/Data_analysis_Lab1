44693369a39cebb8fcde64df8c3336bb4f4ed0df
KA5NTAH <ashilkov2000@mail.ru>
2022-03-07 15:23:19 +0500

    Added comments, fixed cropped plots

--- main.py
+++ main.py
@@ -16,54 +16,11 @@ ASSOC_RULES_THRESHOLDS = np.concatenate((np.array([0.2, 0.3, 0.35, 0.4]), np.ara
 
 os.makedirs(MARKET_BASKET_RESULTS, exist_ok=True)
 os.makedirs(ACCIDENTS_RESULTS, exist_ok=True)
-"""
-Разработайте программу, которая выполняет поиск частых наборов объектов в заданном наборе данных с помощью
-алгоритма Apriori (или одной из его модификаций). Список результирующих наборов должен содержать как наборы,
-так и значение поддержки для каждого набора. Параметрами программы являются набор, порог поддержки и способ
-упорядочивания результирующего списка наборов (по убыванию значения поддержки или лексикографическое).
-Проведите эксперименты на двух наборах из различных предметных областей. Наборы данных должны существенно отличаться
- друг от друга по количеству транзакций и/или типичной длине транзакции (количеству объектов). 
-Например, наборы retail (сведения о покупках в супермаркете: см. html, скачать gzip-архив) и 
-accidents (сведения о ДТП: см. детальное описание в формате PDF, см. html, скачать gzip-архив).
- В экспериментах варьируйте пороговое значение поддержки (например: 1%, 3%, 5%, 10%, 15%).
-
-Выполните визуализацию результатов экспериментов в виде следующих диаграмм:
-сравнение быстродействия на фиксированном наборе данных при изменяемом пороге поддержки;
-количество частых наборов объектов различной длины на фиксированном наборе данных
- при изменяемом пороге поддержки.
- 
-Подготовьте отчет о выполнении задания и загрузите отчет в формате PDF в систему.
-Отчет должен представлять собой связный и структурированный документ со следующими разделами:  
-формулировка задания;
- 
-гиперссылка на каталог репозитория с исходными текстами, наборами данных и др. сопутствующими материалами; 
-рисунки с результатами визуализации; 
-пояснения, раскрывающие смысл полученных результатов.
-"""
-
-
-def test(support_thrd, sorted_method='sup'):
-    dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
-               ['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
-               ['Milk', 'Apple', 'Kidney Beans', 'Eggs'],
-               ['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'],
-               ['Corn', 'Onion', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs']]
-    encoder = TransactionEncoder()
-    encoded_data = encoder.fit(dataset).transform(dataset)
-    dataframe = pd.DataFrame(encoded_data, columns=encoder.columns_)
-    print(dataframe)
-
-    report = apriori(dataframe, min_support=support_thrd, use_colnames=True)
-
-    if sorted_method == 'sup':
-        support = report['support']
-        support_order = np.argsort(support)
-        report = report.reindex(report.index[support_order])
-        print(report)
-    return report
 
 
 def plot_graph(x_points, y_points, savepath, xlabel, ylabel):
+    # plots graph
+    fig = plt.figure(figsize=(15, 8))
     plt.plot(x_points, y_points, marker='o')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -77,6 +34,7 @@ def get_frequent_patterns_from_dataframe(dataframe, support_thrd, sorting_rule="
     if sorting_rule == 'support':
         report = report.sort_values(by=['support'], ascending=False)
     else:
+        # itemsets are stored as the stored sets, so cast to tuple is required for correct sorting
         report = report.sort_values(by=['itemsets'], key = lambda x: tuple(x))        
     return report
 
@@ -84,12 +42,14 @@ def get_frequent_patterns_from_dataframe(dataframe, support_thrd, sorting_rule="
 def get_association_rules(frequent_patterns, confidence_thrd, sorting_rule='support'):
     assert sorting_rule in ['lexic', 'support']
     assoc_rules = association_rules(frequent_patterns, metric='confidence', min_threshold=confidence_thrd)
+    # keep only columns that are required in task description
     assoc_rules = assoc_rules[['antecedents', 'consequents', 'support', 'confidence']]
     if sorting_rule == 'support':
         assoc_rules = assoc_rules.sort_values(by=['support'], ascending=False)
     else:
         assoc_rules = assoc_rules.sort_values(by=['antecedents', 'consequents'], key = lambda x: tuple(x))
     return assoc_rules
+
 
 def save_diagrams(dataframe, root_dir):
     found_sets_nums = []
@@ -124,9 +84,7 @@ def association_rules_diagrams(dataframe, min_support_thrd, root_dir):
     plot_graph(ASSOC_RULES_THRESHOLDS, found_rules_nums, found_rules_sheme_path,
                "confidence threshold", f"found rules, support = {min_support_thrd}")
     plot_graph(ASSOC_RULES_THRESHOLDS, spent_time_stat, time_scheme_path, 
-               "confidence threshold", "spent_time")
-        
-
+               "confidence threshold", "spent_time")        
 
 
 def get_market_basket_analysis(demo=False, sorting_rule='support'):
@@ -144,7 +102,7 @@ def get_market_basket_analysis(demo=False, sorting_rule='support'):
         rules = get_association_rules(frequent_itemsets, 0.1, sorting_rule=sorting_rule)        
         print(f"Association rules:\n {rules}")
     else:
-        # save_diagrams(dataframe, MARKET_BASKET_RESULTS)
+        save_diagrams(dataframe, MARKET_BASKET_RESULTS)
         association_rules_diagrams(dataframe, 0.05, MARKET_BASKET_RESULTS)
 
 
@@ -154,6 +112,8 @@ def get_road_traffic_accidents(demo=False, sorting_rule='support'):
     dataframe = dataframe.iloc[:20000]
     used_cols = ['Severity', 'Street', 'City', 'State', 'Weather_Condition']
     dataframe = dataframe[used_cols]
+
+    #  Mlxtend library requires data to be presented in the list of lists form 
     list_of_values = dataframe.values.tolist()
     for i in range(len(list_of_values)):
         list_of_values[i][0] = 'Severity ' + str(list_of_values[i][0])
@@ -172,13 +132,17 @@ def get_road_traffic_accidents(demo=False, sorting_rule='support'):
         rules = get_association_rules(frequent_itemsets, 0.1, sorting_rule=sorting_rule)        
         print(f"Association rules:\n {rules}")
     else:
-        # save_diagrams(prepared_dataframe, ACCIDENTS_RESULTS)
+        save_diagrams(prepared_dataframe, ACCIDENTS_RESULTS)
         association_rules_diagrams(prepared_dataframe, 0.05, ACCIDENTS_RESULTS)
 
 
-
 if __name__ == "__main__":
+    """
+    Both functions work in 2 modes: 
+    if demo is True then results are printed 
+    else diagrams are plotted and saved 
+    """
     print("MARKET DATASET")
-    get_market_basket_analysis(demo=False, sorting_rule='lexic')
+    get_market_basket_analysis(demo=True, sorting_rule='support')
     print("ROAD TRAFFIC DATASET")
-    get_road_traffic_accidents(demo=False, sorting_rule='lexic')
+    get_road_traffic_accidents(demo=True, sorting_rule='lexic')
